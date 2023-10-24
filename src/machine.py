90d7d149d9059abd5f523358ac9477de57e0343b
Juan Urdaneta <jurdanetac10@gmail.com>
2023-10-24 18:26:51 -0400

    Add final resulting key for character mapping used
    Since the operation of an Enigma machine enciphering
    a message is a series of such configurations, each
    associated with a single character being enciphered,
    a sequence of such representations can be used to
    represent the operation of the machine as it enciphers
    a message.
    
    - Define function to get the key used
    - Remove temporary rotor key on output
    - Make output printing code more readable

--- src/machine.py
+++ src/machine.py
@@ -3,6 +3,7 @@
 """TODO"""
 
 from dataclasses import dataclass
+from io import StringIO
 from string import ascii_uppercase
 
 from rotors import Rotor, StaticRotor
@@ -19,11 +20,40 @@ class Enigma:
     # keep track of how many layers of encryption a letter pass
     encryptions: int = 1
 
+    def _get_key(
+        self,
+        rotor: StaticRotor | Rotor,  # rotor to use
+        key: str = ascii_uppercase,  # assume normal A-Z on default
+        reverse: bool = False,  # whether the current is flowing back
+    ) -> str:
+        """Get the character mappings for a given configuration of the machine"""
+
+        # TODO allow passing machine state as args, this would allow user
+        # access to the function instead of only using it when encrypting,
+        # though the creation of a new local rotor will be needed
+
+        # hold output in another variable, since python strings are immutable
+        new_key: str = ""
+
+        # the enciphering of a character resulting from the application of a
+        # given component's mapping serves as the input to the mapping of the
+        # subsequent component
+        for key_letter in key:
+            # case current flowing backwards
+            if reverse and isinstance(rotor, Rotor):
+                new_key += rotor.reverse_encrypt_letter(key_letter)
+            else:
+                new_key += rotor.encrypt_letter(key_letter)
+
+        return new_key
+
     def encrypt(self, plaintext: str, verbose: bool = False) -> str:
         """TODO"""
 
         # input prompt
         cyphertext: str = ""
+        # resulting character mapping key
+        key: str = ascii_uppercase
 
         for letter in plaintext:
             # not a letter
@@ -47,32 +77,60 @@ class Enigma:
             # substitute letter in plugboard
             cypher_letter: str = self.plugboard.encrypt_letter(letter)
 
+            # get key for keyboard-plugboard mapping
+            key = self._get_key(rotor=self.plugboard, key=key, reverse=False)
+
             # rotor encryption
             for rotor in self.rotors:
                 cypher_letter: str = rotor.encrypt_letter(cypher_letter)
 
+                # update key for plugboard-rotor mapping
+                key = self._get_key(rotor=rotor, key=key, reverse=False)
+
             # reflect letter
             cypher_letter: str = self.reflector.encrypt_letter(cypher_letter)
+
+            # update key for rotor-reflector mapping
+            key = self._get_key(rotor=self.reflector, key=key, reverse=False)
 
             # current flowing in reverse direction
             for rotor in reversed(self.rotors):
                 cypher_letter: str = rotor.reverse_encrypt_letter(cypher_letter)
 
+                # update key for reflector-rotor mapping
+                key = self._get_key(rotor=rotor, key=key, reverse=True)
+
             # substitute letter in plugboard
             cypher_letter: str = self.plugboard.encrypt_letter(cypher_letter)
 
+            # final update key for rotor-plugboard mapping
+            key = self._get_key(rotor=self.plugboard, key=key, reverse=False)
+
+            # append encrypted letter to enciphered text
             cyphertext += cypher_letter
 
             if verbose:
-                tops: str = "".join(
-                    [rotor.current_top for rotor in reversed(self.rotors)]
-                )
-                tops_ascii: str = " ".join([f"{(ord(top) - 64)}" for top in tops])
+                # list of current top character of each rotor
+                tops: list[str] = [rotor.current_top for rotor in reversed(self.rotors)]
+                # list of positions of current top character of each rotor
+                tops_positions: list[str] = []
 
-                # TODO put correct key
-                print(
-                    f"{self.encryptions:03} {letter} > {self.rotors[0].key[:self.rotors[0].key.index(letter)]}({letter}){self.rotors[0].key[self.rotors[0].key.index(letter):]} {tops} {tops_ascii}"
-                )
+                # populate tops_positions list
+                for top in tops:
+                    # calculate position; starting from A = 01, B = 02...
+                    top_position: int = ord(top) - 64
+                    # print to temporary variable formatted number
+                    tmp: StringIO = StringIO()
+                    print(f"{top_position:02}", file=tmp, end=" ")
+                    # add calculated position to list
+                    tops_positions.append(tmp.getvalue())
+
+                # resulting key mapping for machine state on this encryption
+                fmt_key: str = f"{key[:key.index(cypher_letter)]}({cypher_letter}){key[key.index(cypher_letter)+1:]}"
+
+                # Log of how and what encryption was performed
+                output: str = f"{self.encryptions:03} {letter} > {fmt_key} {''.join(tops)} {''.join(tops_positions)}"
+                print(output)
 
             self.encryptions += 1
 
