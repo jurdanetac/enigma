02882756c24b270f2411cb0f6a0ff22fcce1feba
Juan Urdaneta <jurdanetac10@gmail.com>
2023-11-01 15:36:00 -0400

    Fix wrong computation of key mappings
    - Fix wrongly computed key function
    - Add option to print all layers of encrpytion

--- src/machine.py
+++ src/machine.py
@@ -23,23 +23,15 @@ class Enigma:
     def _get_key(
         self,
         rotor: StaticRotor | Rotor,  # rotor to use
-        key: str = ascii_uppercase,  # assume normal A-Z on default
         reverse: bool = False,  # whether the current is flowing back
     ) -> str:
-        """Get the character mappings for a given configuration of the machine"""
-
-        # TODO allow passing machine state as args, this would allow user
-        # access to the function instead of only using it when encrypting,
-        # though the creation of a new local rotor will be needed
+        """Get the character mappings for a given rotor"""
 
         # hold output in another variable, since python strings are immutable
         new_key: str = ""
 
-        # the enciphering of a character resulting from the application of a
-        # given component's mapping serves as the input to the mapping of the
-        # subsequent component
-        for key_letter in key:
-            # case current flowing backwards
+        for key_letter in ascii_uppercase:
+            # case current flowing backwards, rotors after reflector
             if reverse and isinstance(rotor, Rotor):
                 new_key += rotor.reverse_encrypt_letter(key_letter)
             else:
@@ -47,19 +39,36 @@ class Enigma:
 
         return new_key
 
+    def _fmt_key(self, key: str, cypher_letter: str, indicator: str = "") -> str:
+        """Format key"""
+
+        key = key.strip()
+
+        if not key or not isinstance(key, str):
+            return ""
+
+        key_first_half: str = key[: key.index(cypher_letter)]
+        key_second_half: str = key[key.index(cypher_letter) + 1 :]
+        fmt_key: str = f"{indicator} {key_first_half}({cypher_letter}){key_second_half}"
+
+        return fmt_key.strip()
+
     def encrypt(self, plaintext: str, verbose: bool = False) -> str:
         """TODO"""
 
         # input prompt
         cyphertext: str = ""
-        # resulting character mapping key
-        key: str = ascii_uppercase
 
         for letter in plaintext:
             # not a letter
             if letter not in ascii_uppercase:
                 cyphertext += letter
                 continue
+
+            # resulting character mapping key
+            key: str = ascii_uppercase
+            # list of used keys throughout encryption of letter
+            keys_used: list[str] = []
 
             # TODO make rotor turning dynamic
             # TODO support double notch
@@ -74,37 +83,68 @@ class Enigma:
             # right-most rotor turns on every key press
             self.rotors[0].turn()
 
+            # the enciphering of a character resulting from the application of
+            # a given component's mapping serves as the input to the mapping of
+            # the subsequent component
+            cypher_letter: str = letter
+
+            keys_used.append(self._fmt_key(key=key, cypher_letter=cypher_letter))
+
             # substitute letter in plugboard
             cypher_letter: str = self.plugboard.encrypt_letter(letter)
 
             # get key for keyboard-plugboard mapping
-            key = self._get_key(rotor=self.plugboard, key=key, reverse=False)
+            key = self._get_key(rotor=self.plugboard, reverse=False)
 
             # rotor encryption
-            for rotor in self.rotors:
-                cypher_letter: str = rotor.encrypt_letter(cypher_letter)
+            for i, rotor in enumerate(self.rotors):
+                keys_used.append(
+                    self._fmt_key(
+                        key=key, cypher_letter=cypher_letter, indicator=str(i + 1)
+                    ),
+                )
 
-                # update key for plugboard-rotor mapping
-                key = self._get_key(rotor=rotor, key=key, reverse=False)
+                cypher_letter: str = rotor.encrypt_letter(cypher_letter)
+                # update key for rotor-rotor mapping
+                key = self._get_key(rotor=rotor, reverse=False)
+
+            keys_used.append(
+                self._fmt_key(key=key, cypher_letter=cypher_letter, indicator="R")
+            )
 
             # reflect letter
             cypher_letter: str = self.reflector.encrypt_letter(cypher_letter)
 
             # update key for rotor-reflector mapping
-            key = self._get_key(rotor=self.reflector, key=key, reverse=False)
+            key = self._get_key(rotor=self.reflector, reverse=False)
 
             # current flowing in reverse direction
-            for rotor in reversed(self.rotors):
-                cypher_letter: str = rotor.reverse_encrypt_letter(cypher_letter)
+            for i, rotor in enumerate(reversed(self.rotors)):
+                keys_used.append(
+                    self._fmt_key(
+                        key=key, cypher_letter=cypher_letter, indicator=str(i + 1)
+                    )
+                )
 
+                cypher_letter: str = rotor.reverse_encrypt_letter(cypher_letter)
                 # update key for reflector-rotor mapping
-                key = self._get_key(rotor=rotor, key=key, reverse=True)
+                key = self._get_key(rotor=rotor, reverse=True)
+
+            keys_used.append(
+                self._fmt_key(key=key, cypher_letter=cypher_letter, indicator="P")
+            )
 
             # substitute letter in plugboard
             cypher_letter: str = self.plugboard.encrypt_letter(cypher_letter)
-
             # final update key for rotor-plugboard mapping
-            key = self._get_key(rotor=self.plugboard, key=key, reverse=False)
+            key = self._get_key(rotor=self.plugboard, reverse=False)
+
+            keys_used.append(
+                self._fmt_key(
+                    key=key,
+                    cypher_letter=cypher_letter,
+                )
+            )
 
             # append encrypted letter to enciphered text
             cyphertext += cypher_letter
@@ -125,17 +165,18 @@ class Enigma:
                     # add calculated position to list
                     tops_positions.append(tmp.getvalue())
 
-                # resulting key mapping for machine state on this encryption
-                fmt_key: str = f"{key[:key.index(cypher_letter)]}({cypher_letter}){key[key.index(cypher_letter)+1:]}"
-
                 # Log of how and what encryption was performed
-                output: str = f"{self.encryptions:03} {letter} > {fmt_key} {''.join(tops)} {''.join(tops_positions)}"
+                print(f"K > {keys_used[0]}")
+                for key in keys_used[1:-1]:
+                    print(f"  {key}")
+                print(f"{letter} < {keys_used[-1]}")
+
+                print()
+
+                output: str = f"{self.encryptions:04} {letter} < {keys_used[-1]} {''.join(tops)} {''.join(tops_positions)}"
                 print(output)
 
             # increment encrypted-letter count by one
             self.encryptions += 1
-
-            # reset key to prevent wrong carryover to the next encryption
-            key = ascii_uppercase
 
         return cyphertext
